22bd355965abc14164f387968f621b17e80655c2
Vili Sihvo <vilisihvo25@gmail.com>
2024-04-27 14:09:46 +0300

    lomakkeeseen lisätty puuttuvat kentät ja testimielssä logattu syntyvä objekti

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,67 @@ import Lisaa from './components/lisaa'
 function App() {
   const [artikkelit, setArtikkelit] = useState([])
   const [newKey, setNewKey] = useState('')
+  //tästä eteenpäin on uutta
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newJournal, setNewJournal] = useState('')
+  const [newYear, setNewYear] = useState('')
+  const [newVolume, setNewVolume] = useState('')
+  const [newPages, setNewPages] = useState('')
 
   const lisaaArtikkeli = (event) => {
     event.preventDefault()    
     const artikkeliObject = {
       key: newKey,
-      author:null,
-      title:null,
-      journal:null,
-      year:null,
-      volume:null,
-      pages:null,
+      author: newAuthor,
+      title: newTitle,
+      journal: newJournal,
+      year: newYear,
+      volume: newVolume,
+      pages: newPages,
     }
-  
+    
+    console.log(artikkeliObject)
+    
     setArtikkelit(artikkelit.concat(artikkeliObject))
     setNewKey('')
+    setNewAuthor('')
+    setNewTitle('')
+    setNewJournal('')
+    setNewYear('')
+    setNewVolume('')
+    setNewPages('')
+
   }
 
   const handleKeyChange = (event) => {
     setNewKey(event.target.value)
+    console.log("handle key")
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+    console.log("handle key")
+  }
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleJournalChange = (event) => {
+    setNewJournal(event.target.value)
+  }
+
+  const handleYearChange = (event) => {
+    setNewYear(event.target.value)
+  }
+
+  const handleVolumeChange = (event) => {
+    setNewVolume(event.target.value)
+  }
+
+  const handlePagesChange = (event) => {
+    setNewPages(event.target.value)
   }
 
   return (
@@ -34,6 +76,18 @@ function App() {
           lisaaArtikkeli={lisaaArtikkeli} 
           newKey={newKey}
           handleKeyChange={handleKeyChange}
+          newAuthor={newAuthor}
+          handleAuthorChange={handleAuthorChange}
+          newTitle={newTitle}
+          handleTitleChange={handleTitleChange}
+          newJournal={newJournal}
+          handleJournalChange={handleJournalChange}
+          newYear={newYear}
+          handleYearChange={handleYearChange}
+          newVolume={newVolume}
+          handleVolumeChange={handleVolumeChange}
+          newPages={newPages}
+          handlePagesChange={handlePagesChange}
         />
         <h2>Lähteet</h2>
     </>
--- src/components/lisaa.jsx
+++ src/components/lisaa.jsx
@@ -1,24 +1,31 @@
-const Lisaa = ({lisaaArtikkeli, newKey, handleKeyChange}) => {
+const Lisaa = ({lisaaArtikkeli, newKey, handleKeyChange,
+                newAuthor, handleAuthorChange,
+                newTitle, handleTitleChange,
+                newJournal, handleJournalChange,
+                newYear, handleYearChange,
+                newVolume, handleVolumeChange,
+                newPages, handlePagesChange
+}) => {
     return(
     <form onSubmit={lisaaArtikkeli}> 
         <div>
           key: <input value={newKey} onChange={handleKeyChange}/>
         </div>
         <div>
-          author:
+          author: <input value={newAuthor} onChange={handleAuthorChange}/>
         </div>
         <div>
-          title: 
+          title: <input value={newTitle} onChange={handleTitleChange}/>
         </div>
-          journal:
+          journal: <input value={newJournal} onChange={handleJournalChange}/>
         <div>
-          year:
-        </div>
-        <div>
-          volume:
+          year: <input value={newYear} onChange={handleYearChange}/>
         </div>
         <div>
-          pages:
+          volume: <input value={newVolume} onChange={handleVolumeChange}/>
+        </div>
+        <div>
+          pages: <input value={newPages} onChange={handlePagesChange}/>
         </div>
         <div>
           <button type="submit">lisää</button>
