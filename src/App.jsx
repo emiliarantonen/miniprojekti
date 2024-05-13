b8200e553530942a4de7a888dbd69fe64c510e2d
lehtio <laura.l.lehtio@gmail.com>
2024-05-13 14:40:50 +0300

    artikkelit aakkosjärjestykseen kirjoittajan sukunimen mukaan

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   }
 
   // artikkelien järjestys kirjoittajan sukunimen perusteella
-  const sortArticles = () => {
+  const jarjastaArtikkelit = () => {
     const sortedArticles = [...artikkelit].sort((a, b) => {
       const lastNameA = a.author[0].lastName.toLowerCase();
       const lastNameB = b.author[0].lastName.toLowerCase();
@@ -89,7 +89,7 @@ function App() {
         <h2>Lähteet</h2>
         {/* tietokannan tyhennys */}
         <button onClick={clearDatabase}>Tyhjennä tietokanta</button>
-        <button onClick={sortArticles}>Järjestä Artikkelit</button>
+        <button onClick={jarjastaArtikkelit}>Järjestä Artikkelit</button>
         <p>{message}</p>
         {artikkelit.map((artikkeli, indeksi) => (
         <div key={indeksi} className="artikkelituloste">
