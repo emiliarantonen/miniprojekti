427a8d5a85bd9ce5dfe6089a92321a67d77b7aeb
Vili Sihvo <vilisihvo25@gmail.com>
2024-05-14 21:33:42 +0300

    Lisäykseen liittyvät testit tehty.

--- src/components/lisaa.test.js
+++ src/components/lisaa.test.js
@@ -189,7 +189,7 @@ test('Article is added properly when author is entered as \"Firstname Lastname\"
   })
 })
 
-test('Article is added properly when multiple names of the authors are entered', async () => {
+test('Article is added properly when multiple names of the authors are entered as \"Firstname Lastname\" and \"Firstname Lastname\"', async () => {
   const user = userEvent.setup()
   const lisaaArtikkeli = jest.fn()
 
@@ -207,6 +207,50 @@ test('Article is added properly when multiple names of the authors are entered',
   // Täytetään lomake
   await user.type(keyInput, 'testkey')
   await user.type(authorInput, 'test author1 and test author2')
+  await user.type(titleInput, 'test title')
+  await user.type(journalInput, 'test journal')
+  await user.type(yearInput, '2023')
+  await user.type(volumeInput, 'test volume')
+  await user.type(pagesInput, '22-45')
+  await user.click(sendButton)
+
+  //testataan, että lisaaArtikkeli kutsutaan lomakkeen hyväksymisen jälkeen
+  expect(lisaaArtikkeli).toHaveBeenCalledTimes(1)
+  //testataan, että lisaaArtikkeli kutsutaan oikealla parametrilla
+  expect(lisaaArtikkeli).toHaveBeenCalledWith({
+    articleKey: 'testkey',
+    author: [{firstName: "test",
+              lastName: "author1"
+             }, 
+             {firstName: "test",
+              lastName: "author2"
+             }],
+    title: 'test title',
+    journal: 'test journal',
+    year: '2023',
+    volume: 'test volume',
+    pages: '22-45',
+  })
+})
+
+test('Article is added properly when multiple names of the authors are entered as \"Firstname Lastname\" and \"Lastname, Firstname\"', async () => {
+  const user = userEvent.setup()
+  const lisaaArtikkeli = jest.fn()
+
+  const { container } = render(<Lisaa createArtikkeli={lisaaArtikkeli} />)  
+
+  const keyInput = container.querySelector('#key-input')
+  const authorInput = container.querySelector('#author')
+  const titleInput = container.querySelector('#title')
+  const journalInput = container.querySelector('#journal')
+  const yearInput = container.querySelector('#year')
+  const volumeInput = container.querySelector('#volume')
+  const pagesInput = container.querySelector('#pages')
+  const sendButton = container.querySelector('#lisaa-button')
+
+  // Täytetään lomake
+  await user.type(keyInput, 'testkey')
+  await user.type(authorInput, 'test author1 and author2, test')
   await user.type(titleInput, 'test title')
   await user.type(journalInput, 'test journal')
   await user.type(yearInput, '2023')
