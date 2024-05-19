751000f0d47c8c41b3cbd291412d724f89dd2a26
Vili Sihvo <vilisihvo25@gmail.com>
2024-05-19 19:31:41 +0300

    Kirjan lisääminen toteutettu.

--- src/components/lisaa.test.js
+++ src/components/lisaa.test.js
@@ -67,47 +67,6 @@ test('Lisaa displays alert when not all fields are filled', async () => {
 })
 
 test('Article is added properly when author is entered as \"Lastname, Firstname\"', async () => {
-    const user = userEvent.setup()
-    const lisaaArtikkeli = jest.fn()
-
-    const { container } = render(<Lisaa createArtikkeli={lisaaArtikkeli} />)  
-
-    const keyInput = container.querySelector('#key-input')
-    const authorInput = container.querySelector('#author')
-    const titleInput = container.querySelector('#title')
-    const journalInput = container.querySelector('#journal')
-    const yearInput = container.querySelector('#year')
-    const volumeInput = container.querySelector('#volume')
-    const pagesInput = container.querySelector('#pages')
-    const sendButton = container.querySelector('#lisaa-button')
-  
-    // Täytetään lomake
-    await user.type(keyInput, 'testkey')
-    await user.type(authorInput, 'test, author')
-    await user.type(titleInput, 'test title')
-    await user.type(journalInput, 'test journal')
-    await user.type(yearInput, '2023')
-    await user.type(volumeInput, 'test volume')
-    await user.type(pagesInput, '22-45')
-    await user.click(sendButton)
-
-    //testataan, että lisaaArtikkeli kutsutaan lomakkeen hyväksymisen jälkeen
-    expect(lisaaArtikkeli).toHaveBeenCalledTimes(1)
-    //testataan, että lisaaArtikkeli kutsutaan oikealla parametrilla
-    expect(lisaaArtikkeli).toHaveBeenCalledWith({
-      articleKey: 'testkey',
-      author: [{firstName: "author",
-        lastName: "test"
-      }],
-      title: 'test title',
-      journal: 'test journal',
-      year: '2023',
-      volume: 'test volume',
-      pages: '22-45',
-    })
-})
-
-test('Article is added properly when author is entered as \"Lastname, Firstname\"', async () => {
   const user = userEvent.setup()
   const lisaaArtikkeli = jest.fn()
 
