da18d65b50a2f7ecff253377a5160d3d3991af2f
lehtio <laura.l.lehtio@gmail.com>
2024-05-21 21:18:55 +0300

    doi-haun tulostamisen testaus

--- src/components/doi.test.js
+++ src/components/doi.test.js
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event'
 import axios from 'axios'
 import DOI from './doi'
 
+
 jest.mock('axios')
 
 // Mockataan data hakemisen sijalle
@@ -98,5 +99,39 @@ test('Article is not added when article key isn\'t entered', async () => {
 
     await waitFor(() => {
         expect(lisaaDOI).toHaveBeenCalledTimes(0)
+    })
+})
+
+// testataan ettei lisää jos key on tupla
+test('Article is not added when article key is duplicate', async () => {
+    const user = userEvent.setup()
+    const lisaaDOI = jest.fn()
+    window.alert = jest.fn()
+
+    // Mock axios jotta palautetaan oikea data
+    axios.get.mockResolvedValueOnce({ data: mockData })
+
+    
+    const existingKeys = ['existingKey']
+    const { container } = render(<DOI createByDOI={(ref) => {
+        if (existingKeys.includes(ref.articleKey)) {
+            window.alert('The article key already exists. Please use a unique key.')
+            return
+        }
+        lisaaDOI(ref)
+    }} />)
+
+    const keyInput = container.querySelector("#key-input")
+    const doiInput = container.querySelector("#doi-input")
+    const haeButton = container.querySelector("#hae-button")
+
+        // testataan tuplakeylla
+    await user.type(keyInput, 'existingKey')
+    await user.type(doiInput, '10.1111/j.1365-201x.2005.01464.x')
+    await user.click(haeButton)
+
+    await waitFor(() => {
+        expect(lisaaDOI).toHaveBeenCalledTimes(0)
+        expect(window.alert).toHaveBeenCalledWith('The article key already exists. Please use a unique key.')
     })
 })
